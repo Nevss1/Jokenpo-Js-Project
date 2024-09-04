23132f61d54be1af2b52d6b90148e36d48c56e03
Neves <rafaelnvss002@gmail.com>
2024-09-04 15:08:46 -0300

    feat: added autoplay implementation

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,82 +1,103 @@
 const score = JSON.parse(localStorage.getItem
-    ('score')) || {
-        wins: 0,
-        losses: 0,
-        ties: 0
-    }
-    let randomNumber = -1;
-    let computerMove = '';
+('score')) || {
+    wins: 0,
+    losses: 0,
+    ties: 0
+}
+let randomNumber = -1;
+let computerMove = '';
 
-    updateScoreElement();
+updateScoreElement();
 
-    function updateScoreElement(){
-        document.querySelector('.js-score')
-            .innerHTML = `Wins: ${score.wins}, 
-            Losses ${score.losses}, ties ${score.ties}`
-    }
+function updateScoreElement(){
+    document.querySelector('.js-score')
+        .innerHTML = `Wins: ${score.wins}, 
+        Losses ${score.losses}, ties ${score.ties}`
+}
 
-    function jokenpo(plrMove, randnum){
-        randomNumber = Math.random();
-        let result = '';
-        if (plrMove == 'rock') {
-            if (randnum <= 1/3) {
-                pcMove = 'rock';
-                result = 'Tie!'
-                score.ties += 1;
-            } else if (1/3 < randnum && randnum <= 2/3) {
-                pcMove = 'paper';
-                result = 'You Lose!';
-                score.losses += 1;
-            } else if (randnum > 2/3) {
-                pcMove = 'Scissors';
-                result = 'You Win!';
-                score.wins += 1;
-            }
-
-        } else if (plrMove == 'paper') {
-            if (randnum <= 1/3) {
-                pcMove = 'rock';
-                result = 'You Win!';
-                score.wins += 1;
-            } else if (1/3 < randnum && randnum <= 2/3) {
-                pcMove = 'paper';
-                result = 'Tie!'
-                score.ties += 1;
-            } else if (randnum > 2/3) {
-                pcMove = 'Scissors';
-                result = 'You Lose!';
-                score.losses += 1;
-            }
-
-        } else if (plrMove == 'scissors') {
-            if (randnum <= 1/3) {
-                pcMove = 'rock';
-                result = 'You Lose!';
-                score.losses += 1;
-            } else if (1/3 < randnum && randnum <= 2/3) {
-                pcMove = 'paper';
-                result = 'You Win!';
-                score.wins += 1;
-            } else if (randnum > 2/3) {
-                pcMove = 'Scissors';
-                result = 'Tie!'
-                score.ties += 1;
-            }
+function jokenpo(plrMove){
+    randnum = Math.random();
+    let result = '';
+    if (plrMove == 'rock') {
+        if (randnum <= 1/3) {
+            pcMove = 'rock';
+            result = 'Tie!'
+            score.ties += 1;
+        } else if (1/3 < randnum && randnum <= 2/3) {
+            pcMove = 'paper';
+            result = 'You Lose!';
+            score.losses += 1;
+        } else if (randnum > 2/3) {
+            pcMove = 'Scissors';
+            result = 'You Win!';
+            score.wins += 1;
         }
 
-        document.querySelector('.js-result')
-            .innerHTML = result;
+    } else if (plrMove == 'paper') {
+        if (randnum <= 1/3) {
+            pcMove = 'rock';
+            result = 'You Win!';
+            score.wins += 1;
+        } else if (1/3 < randnum && randnum <= 2/3) {
+            pcMove = 'paper';
+            result = 'Tie!'
+            score.ties += 1;
+        } else if (randnum > 2/3) {
+            pcMove = 'Scissors';
+            result = 'You Lose!';
+            score.losses += 1;
+        }
 
-        document.querySelector('.your-move')
-            .innerHTML = `
-            You
-            <img src="emojis-icons/${plrMove}-emoji.png" class="result-icon">`;
+    } else if (plrMove == 'scissors') {
+        if (randnum <= 1/3) {
+            pcMove = 'rock';
+            result = 'You Lose!';
+            score.losses += 1;
+        } else if (1/3 < randnum && randnum <= 2/3) {
+            pcMove = 'paper';
+            result = 'You Win!';
+            score.wins += 1;
+        } else if (randnum > 2/3) {
+            pcMove = 'Scissors';
+            result = 'Tie!'
+            score.ties += 1;
+        }
+    }
+    document.querySelector('.js-result')
+        .innerHTML = result;
 
-            document.querySelector('.pc-move')
-            .innerHTML = `
-            <img src="emojis-icons/${pcMove}-emoji.png" class="result-icon">
-            Computer`;
-        updateScoreElement();
+    document.querySelector('.your-move')
+        .innerHTML = `
+        You
+        <img src="emojis-icons/${plrMove}-emoji.png" class="result-icon">`;
 
-        localStorage.setItem('score', JSON.stringify(score));
-    };
+        document.querySelector('.pc-move')
+        .innerHTML = `
+        <img src="emojis-icons/${pcMove}-emoji.png" class="result-icon">
+        Computer`;
+    updateScoreElement();
+
+    localStorage.setItem('score', JSON.stringify(score));
+};
+
+let isAutoPlaying = false;
+let intervalId;
+
+function autoPlay() {
+    if (!isAutoPlaying){
+        intervalId = setInterval(function () {
+            randnum = Math.random();
+            if (randnum < 1/3) {
+                jokenpo('rock');
+            } else if (1/3 < randnum && randnum <= 2/3) {
+                jokenpo('paper');
+            } else if (randnum > 2/3) {
+                jokenpo('scissors');
+            }
+        }, 1500);
+        isAutoPlaying = true;
+    } else {
+        clearInterval(intervalId);
+        isAutoPlaying = false
+    }
+}
